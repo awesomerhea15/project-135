80b8477171c079e51831edb4d4f775bf625cf452
awesomerhea15 <92613200+awesomerhea15@users.noreply.github.com>
2023-01-29 18:23:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw(){
+    image(video, 0, 0, 500, 500)
+
+}
